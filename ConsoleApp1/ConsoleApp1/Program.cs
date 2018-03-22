fc8f4f84d0d7d34f9294480fd2bbcd04e59ba575
Admin <Admin@DESKTOP-INJ064T>
2018-03-22 13:56:11 +0700

    master

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите A");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите B");
+            int b = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
